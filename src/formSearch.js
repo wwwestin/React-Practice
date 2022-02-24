28a8dad32944a756228b69776710db24a8654db9
wwwestin <westin.h@gmail.com>
2022-02-24 14:50:21 -0500

    added skeleton to FormCard and FormFun Components

--- src/formSearch.js
+++ src/formSearch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function FormCard () {
+function FormSearch () {
     return (
         <div>
 
@@ -8,4 +8,4 @@ function FormCard () {
     )
 }
 
-export default FormCard;
+export default FormSearch;
